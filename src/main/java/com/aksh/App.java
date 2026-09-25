f838dc4969c840b5654d12e069dc52cf0e373ebd
AKSH CHAUHAN <aksh111828@gmail.com>
2026-09-25 23:44:18 +0530

    Initial Maven project for Jenkins Practical 5

---
+++ src/main/java/com/aksh/App.java
@@ -0,0 +1,12 @@
+package com.aksh;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("CI/CD Practical 5 - Jenkins");
+        System.out.println("Build executed successfully!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
